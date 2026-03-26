270b33d0b0fc662c4751ed36dff5b8d4665fe10b
Chirag Patel <chirag.patel.learning@gmail.com>
2026-03-26 12:02:25 -0500

    Comments removed from Tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -21,7 +21,6 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638,-84.677017,Taco Bell Acwort...",-84.677017)]
-        //Add additional inline data. Refer to your CSV file.
         [InlineData("33.671982,-85.826674,Taco Bell Annisto...", -85.826674)]
         [InlineData("33.587217,-85.057114,Taco Bell Carrollto...", -85.057114)]
         [InlineData("30.22841,-85.649286,Taco Bell Lynn Have...", -85.649286)]
@@ -29,12 +28,7 @@ namespace LoggingKata.Test
         [InlineData("32.524115,-86.20775,Taco Bell Wetumpk...", -86.20775)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
+        
             //Arrange
             TacoParser tacoParser = new TacoParser();
 
@@ -46,10 +40,8 @@ namespace LoggingKata.Test
         }
 
 
-        //TODO: Create a test called ShouldParseLatitude
         [Theory]
         [InlineData("34.073638,-84.677017,Taco Bell Acwort...", 34.073638)]
-        //Add additional inline data. Refer to your CSV file.
         [InlineData("33.671982,-85.826674,Taco Bell Annisto...", 33.671982)]
         [InlineData("33.587217,-85.057114,Taco Bell Carrollto...", 33.587217)]
         [InlineData("30.22841,-85.649286,Taco Bell Lynn Have...", 30.22841)]
@@ -57,11 +49,6 @@ namespace LoggingKata.Test
         [InlineData("32.524115,-86.20775,Taco Bell Wetumpk...", 32.524115)]
         public void ShouldParseLatitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Latitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
 
             //Arrange
             TacoParser tacoParser = new TacoParser();
